6ed03a0f838372c7204257d2cbcbd880ff9080a3
andriel2305 <andriellhenrique@gmail.com>
2024-05-07 23:17:06 -0400

    Inserido botao incluir cliente - Criado component client-inclusion

--- front_end_ang/src/app/app.module.ts
+++ front_end_ang/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { ProdutoSaidaComponent } from './produto-saida/produto-saida.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSelectModule }  from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { ClientInclusionComponent } from './client-inclusion/client-inclusion.component';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,7 @@ import { MatButtonModule } from '@angular/material/button';
     ,LoginComponent
     ,ConfigComponent
     ,ProdutoEntradaComponent
-    ,ProdutoSaidaComponent
+    ,ProdutoSaidaComponent, ClientInclusionComponent
 
   ],
   imports: [
---
+++ front_end_ang/src/app/client-inclusion/client-inclusion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-inclusion',
+  templateUrl: './client-inclusion.component.html',
+  styleUrl: './client-inclusion.component.css'
+})
+export class ClientInclusionComponent {
+
+}

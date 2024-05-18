b087f90dcbe78d266b760804823cc212e2e4dc9d
jcmarcussojr <163558954+jcmarcussojr@users.noreply.github.com>
2024-05-18 19:31:55 -0300

    Feito esquema de config pelos arquivos "environment"
    Feito o esquema para poder configurar o projeto com 2 ambientes:
    - production (arquivo environment.ts)
    - environment. (arquivo environment.development.ts)
    Estes arquivos (e suas configs) são usadas quando se executa pelo ng serve (com o param --configurartion) e também quando faz o build de production e de development.

--- front_end_ang/src/app/env.ts
+++ front_end_ang/src/app/env.ts
@@ -1,10 +1,17 @@
 
+// 18/05/2024
+// este arquivo NAO DEVE MAIS SER ALTERADO para "trocar" a configuracao REST_API_URL
+// esta config REST_API_URL deve ser alterada nos arquivos "environment"
+
 // configs, consts e vars do projeto
+
+import { environment } from "../environments/environment";
+
+//console.log("environment.production=" + environment.production);
+//console.log("environment.REST_API_URL=" + environment.REST_API_URL);
 
 export const ENV = {
 
-  //REST_API_URL: 'http://localhost:3000',
-  REST_API_URL: 'http://179.145.6.125:3000',
+  REST_API_URL: environment.REST_API_URL
   
 };
-
---
+++ front_end_ang/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// arquivo padrao que contem configs de PRODUCTION
+export const environment = {
+  production: true,
+  REST_API_URL: "http://179.145.6.125:3000",
+};

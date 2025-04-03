f0602c70100fb5437e86b281a46fff846c199309
Marcos de Lima Bühl <marcoslimabuhl@gmail.com>
2025-04-02 23:48:02 -0300

    refactor: substitui ip pelo domínio
    Os arquivos foram alterados no servidor pelo Zé mas ainda não temos atualizado no repositório

--- front_end_ang/src/environments/environment.ts
+++ front_end_ang/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // arquivo padrao que contem configs de PRODUCTION
 export const environment = {
   production: true,
-  REST_API_URL: "http://179.145.6.125:3000",
+  REST_API_URL: "http://sistech.myftp.org:3000",
   // numero minimo de chars para a senha do usuario
   NumMinCharSenhaUsuario: 8
 };
